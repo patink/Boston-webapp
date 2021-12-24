6fc02eb0cea7d344da3eab1c2390fc7e1ad30761
Nisith Pati <54003224+patink@users.noreply.github.com>
2021-12-25 01:18:38 +0530

    Update price.py

--- price.py
+++ price.py
@@ -1,4 +1,10 @@
-
+import streamlit as st
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+import plotly.express as px
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
